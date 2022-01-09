84c60d955d237b8aa3b0d7b5110a994f60f9363d
Vladislav Nikitin <vlad.nikitin_1991@mail.ru>
2022-01-09 15:06:43 +0300

    fixed problems

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,7 +3,18 @@ const menuLink = document.getElementsByClassName("menu__link");
 
 for (let i = 0; i < menuLink.length; i++) {
   menuLink[i].onclick = function () {
-    menuItem[i].querySelector(".menu_sub").classList.toggle("menu_active");
-    return false;
+    if (
+      menuItem[i].querySelector(".menu_sub") !== null &&
+      menuItem[i].querySelector(".menu_active") === null
+    ) {
+      menuItem[i].querySelector(".menu_sub").classList.add("menu_active");
+      return false;
+    } else if (
+      menuItem[i].querySelector(".menu_sub") !== null &&
+      menuItem[i].querySelector(".menu_active") !== null
+    ) {
+      menuItem[i].querySelector(".menu_sub").classList.remove("menu_active");
+      return false;
+    }
   };
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,17 +1,17 @@
-const sliderItems = document.getElementsByClassName("slider__item");
-const sliderArrow = document.getElementsByClassName("slider__arrow");
-let n = 0;
+const sliderItems = Array.from(document.getElementsByClassName("slider__item"));
+const arrows = Array.from(document.querySelectorAll(".slider__arrow"));
+const sliderArrowPrev = document.querySelector(".slider__arrow_prev");
+const sliderArrowNext = document.querySelector(".slider__arrow_next");
+let index = sliderItems.findIndex((slide) =>
+  slide.classList.contains("slider__item_active")
+);
 
-for (let i = 0; i < sliderArrow.length; i++) {
-  sliderArrow[i].onclick = function () {
-    ++n;
-    if (n >= sliderItems.length) {
-      sliderItems[n - 1].classList.remove("slider__item_active");
-      n = 0;
-      sliderItems[n].classList.add("slider__item_active");
-    } else {
-      sliderItems[n - 1].classList.remove("slider__item_active");
-      sliderItems[n].classList.add("slider__item_active");
-    }
+for (let i = 0; i < arrows.length; i++) {
+  arrows[i].onclick = function () {
+    sliderItems[index].classList.remove("slider__item_active");
+    arrows[i] === sliderArrowNext ? index++ : index--;
+    index === sliderItems.length ? (index = 0) : "";
+    index === -1 ? (index = sliderItems.length - 1) : "";
+    sliderItems[index].classList.add("slider__item_active");
   };
 }
